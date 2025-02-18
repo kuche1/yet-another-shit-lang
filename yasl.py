8ec39477713a8bc5401f1e1dabb548e03cfab9bc
kuche1 <znamsiparolata4esno@abv.bg>
2025-02-19 01:42:21 +0200

    update

--- yasl.py
+++ yasl.py
@@ -6,6 +6,7 @@ import os
 
 HERE = os.path.dirname(os.path.realpath(__file__))
 FOLDER_TMP = os.path.join(HERE, 'tmp')
+FILE_INPUT = os.path.join(HERE, 'test.yasl')
 FILE_EXECUTABLE = os.path.join(FOLDER_TMP, 'executable')
 
 def term(args:list[str]) -> None:
@@ -13,10 +14,11 @@ def term(args:list[str]) -> None:
 
 os.makedirs(FOLDER_TMP, exist_ok=True)
 
-inp = os.path.join(HERE, 'test.yasl')
-out = os.path.join(FOLDER_TMP, 'test.c')
-shutil.copyfile(inp, out)
+with open(FILE_INPUT, 'r') as f_in:
+    file_c = os.path.join(FOLDER_TMP, 'test.c')
+    with open(file_c, 'w') as f_out:
+        f_out.write(f_in.read())
 
-term(['gcc', '-Werror', '-Wextra', '-Wall', '-pedantic', '-Wfatal-errors', '-Wshadow', '-fwrapv', '-o', FILE_EXECUTABLE, out])
+term(['gcc', '-Werror', '-Wextra', '-Wall', '-pedantic', '-Wfatal-errors', '-Wshadow', '-fwrapv', '-o', FILE_EXECUTABLE, file_c])
 
 term([FILE_EXECUTABLE])
