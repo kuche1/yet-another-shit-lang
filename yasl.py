c4d21203dc209a3fc781de0ba0b4d08d4190eb81
kuche1 <znamsiparolata4esno@abv.bg>
2025-02-19 01:43:59 +0200

    update

--- yasl.py
+++ yasl.py
@@ -7,6 +7,7 @@ import os
 HERE = os.path.dirname(os.path.realpath(__file__))
 FOLDER_TMP = os.path.join(HERE, 'tmp')
 FILE_INPUT = os.path.join(HERE, 'test.yasl')
+FILE_TMP_OUTPUT = os.path.join(FOLDER_TMP, 'code.c')
 FILE_EXECUTABLE = os.path.join(FOLDER_TMP, 'executable')
 
 def term(args:list[str]) -> None:
@@ -15,10 +16,11 @@ def term(args:list[str]) -> None:
 os.makedirs(FOLDER_TMP, exist_ok=True)
 
 with open(FILE_INPUT, 'r') as f_in:
-    file_c = os.path.join(FOLDER_TMP, 'test.c')
-    with open(file_c, 'w') as f_out:
-        f_out.write(f_in.read())
+    yasl_code = f_in.read()
 
-term(['gcc', '-Werror', '-Wextra', '-Wall', '-pedantic', '-Wfatal-errors', '-Wshadow', '-fwrapv', '-o', FILE_EXECUTABLE, file_c])
+with open(FILE_TMP_OUTPUT, 'w') as f_out:
+    f_out.write(yasl_code)
+
+term(['gcc', '-Werror', '-Wextra', '-Wall', '-pedantic', '-Wfatal-errors', '-Wshadow', '-fwrapv', '-o', FILE_EXECUTABLE, FILE_TMP_OUTPUT])
 
 term([FILE_EXECUTABLE])
