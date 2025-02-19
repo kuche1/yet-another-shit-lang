0a47a6a0123e21a26bdac9716642231fb44b5bc9
kuche1 <znamsiparolata4esno@abv.bg>
2025-02-19 11:31:53 +0200

    update

--- yasl.py
+++ yasl.py
@@ -28,182 +28,192 @@ TUPLE_END = FN_ARG_END
 
 VAR_NAME_SEPARATORS = WHITESPACE + [FN_ARG_BEGIN, FN_ARG_END] + [VAR_TYPE_SEP] + [TUPLE_BEGIN, TUPLE_END]
 
-def term(args:list[str]) -> None:
-    subprocess.run(args, check=True)
-
-# pop: whitespace
-
-def pop_whitespace(src:str) -> str:
-
-    while True:
-        if len(src) == 0:
-            break
-
-        ch = src[0]
-
-        if ch in WHITESPACE:
-            src = src[1:]
-            continue
-
-        if ch == '/':
-            if len(src) >= 2:
-                if src[1] == '/':
-                    next_newline = src.find('\n')
-                    if next_newline == -1:
-                        next_newline = len(src)
-                    src = src[next_newline+1:]
-                    continue
-
-        break
-
-    return src
-
-# pop: var name
-
-def pop_var_name(src:str, justreturnif:None|str=None) -> tuple[str, str]:
-    src = pop_whitespace(src)
-
-    data = ''
-
-    while True:
-        ch = src[0]
-        src = src[1:]
-
-        if data + ch == justreturnif:
-            data += ch
-            break
-
-        if ch in VAR_NAME_SEPARATORS:
-            src = ch + src
-            break
-
-        data += ch
-
-    assert len(data)
-
-    # this make the resulting C code less readable
-    data = data.replace('-', '$MINUS$')
-    data = data.replace('+', '$PLUS$')
-
-    return src, data
-
-# pop: var name and type
-
-def pop_var_name_and_type(src:str, just_return_if_varname_is:None|str=None) -> tuple[str, str|tuple[str, str]]:
-    src, name = pop_var_name(src, just_return_if_varname_is)
-    if isinstance(just_return_if_varname_is, str):
-        if name == just_return_if_varname_is:
-            return src, just_return_if_varname_is
-    
-    src, sep = pop_var_name(src, justreturnif=VAR_TYPE_SEP)
-    assert sep == VAR_TYPE_SEP
-    
-    src, typ = pop_var_name(src)
-
-    return src, (name, typ)
-
-# pop: var metatype
+# enum
 
 class VarMetatype(enum.Enum):
     FN = 0
     VAR = 1
     VAL = 2
 
-def pop_var_metatype(src:str) -> tuple[str, VarMetatype]:
-    src, metatype = pop_var_name(src)
+# generic fnc
 
-    if metatype == 'fn':
-        mt = VarMetatype.FN
-    elif metatype == 'var':
-        mt = VarMetatype.VAR
-    elif metatype == 'val':
-        mt = VarMetatype.VAL
-    else:
-        raise Exception(f'unknown var metatype `{metatype}`')
+def term(args:list[str]) -> None:
+    subprocess.run(args, check=True)
 
-    return src, mt
+# class
 
-# pop: tuple
+class Src:
 
-def pop_tuple(src:str) -> tuple[str, tuple[str, ...]]:
-    # TODO we're not taking care of string
+    def __init__(self, file:str) -> None:
+        with open(file, 'r') as f:
+            self.src = f.read()
 
-    src, tuple_begin = pop_var_name(src, justreturnif=TUPLE_BEGIN)
-    assert tuple_begin == TUPLE_BEGIN
+    def no_more_code(self) -> bool:
+        return len(self.src) == 0
 
-    the_tuple = []
-    while True:
-        src, item = pop_var_name(src, justreturnif=TUPLE_END)
-        if item == TUPLE_END:
-            break
-        the_tuple.append(item)
-    return src, tuple(the_tuple)
+    # pop: whitespace
 
-# pop: fn name
+    def pop_whitespace(self) -> None:
 
-def pop_fn_name_and_returntype(src:str) -> tuple[str, tuple[str, str]]:
-    src, name_and_returntype = pop_var_name_and_type(src)
-    assert isinstance(name_and_returntype, tuple) # make mypy happy
-    return src, name_and_returntype
+        while True:
+            if len(self.src) == 0:
+                break
 
-def pop_fn_name(src:str, orr:None|str=None) -> tuple[str, str]:
-    return pop_var_name(src, orr)
+            ch = self.src[0]
 
-# pop: fn arg
+            if ch in WHITESPACE:
+                self.src = self.src[1:]
+                continue
 
-def pop_fn_arg_begin(src:str) -> str:
-    src, fn_arg_begin = pop_var_name(src, justreturnif=FN_ARG_BEGIN)
-    assert fn_arg_begin == FN_ARG_BEGIN
-    return src
+            if ch == '/':
+                if len(self.src) >= 2:
+                    if self.src[1] == '/':
+                        next_newline = self.src.find('\n')
+                        if next_newline == -1:
+                            next_newline = len(self.src)
+                        self.src = self.src[next_newline+1:]
+                        continue
 
-def pop_fn_def_arg_or_end(src:str) -> tuple[str, None|tuple[str,str]]:
-    src, name_and_type = pop_var_name_and_type(src, just_return_if_varname_is=FN_ARG_END)
-    if name_and_type == FN_ARG_END:
-        return src, None
-
-    assert isinstance(name_and_type, tuple)
-    # make mypy happy
-    
-    return src, name_and_type
-
-def pop_fn_def_args(src:str) -> tuple[str, tuple[tuple[str,str], ...]]:
-    src = pop_fn_arg_begin(src)
-
-    args = []
-    while True:
-        src, arg = pop_fn_def_arg_or_end(src)
-        if arg is None:
-            break
-        args.append(arg)
-
-    return src, tuple(args)
-
-def pop_fn_call_args(src:str) -> tuple[str, tuple[str, ...]]:
-    return pop_tuple(src)
-
-# pop: fn body
-
-def pop_fn_body_begin(src:str) -> str:
-    src, fn_body_begin = pop_var_name(src, justreturnif=FN_BODY_BEGIN)
-    assert fn_body_begin == FN_BODY_BEGIN
-    return src
-
-def pop_fn_body(src:str) -> tuple[str, str]:
-    src = pop_fn_body_begin(src)
-
-    # TODO missing implementation
-
-    data = ''
-    while True:
-        src, fn_name = pop_fn_name(src, orr=FN_BODY_END)
-        if fn_name == FN_BODY_END:
             break
 
-        src, fn_call_args = pop_fn_call_args(src)
+    # pop: var name
 
-        data += f'{fn_name}({','.join(fn_call_args)});\n'
+    def pop_var_name(self, justreturnif:None|str=None) -> str:
+        self.pop_whitespace()
 
-    return src, data
+        data = ''
+
+        while True:
+            ch = self.src[0]
+            self.src = self.src[1:]
+
+            if data + ch == justreturnif:
+                data += ch
+                break
+
+            if ch in VAR_NAME_SEPARATORS:
+                self.src = ch + self.src
+                break
+
+            data += ch
+
+        assert len(data)
+
+        # this make the resulting C code less readable
+        data = data.replace('-', '$MINUS$')
+        data = data.replace('+', '$PLUS$')
+
+        return data
+
+    # pop: var name and type
+
+    def pop_var_name_and_type(self, just_return_if_varname_is:None|str=None) -> str|tuple[str, str]:
+        name = self.pop_var_name(just_return_if_varname_is)
+        if isinstance(just_return_if_varname_is, str):
+            if name == just_return_if_varname_is:
+                return just_return_if_varname_is
+        
+        assert self.pop_var_name(justreturnif=VAR_TYPE_SEP) == VAR_TYPE_SEP
+        
+        typ = self.pop_var_name()
+
+        return name, typ
+
+    # pop: var metatype
+
+    def pop_var_metatype(self) -> VarMetatype:
+        metatype = self.pop_var_name()
+
+        if metatype == 'fn':
+            mt = VarMetatype.FN
+        elif metatype == 'var':
+            mt = VarMetatype.VAR
+        elif metatype == 'val':
+            mt = VarMetatype.VAL
+        else:
+            raise Exception(f'unknown var metatype `{metatype}`')
+
+        return mt
+
+    # pop: tuple
+
+    def pop_tuple(self) -> tuple[str, ...]:
+        # TODO we're not taking care of string
+
+        tuple_begin = self.pop_var_name(justreturnif=TUPLE_BEGIN)
+        assert tuple_begin == TUPLE_BEGIN
+
+        the_tuple = []
+        while True:
+            item = self.pop_var_name(justreturnif=TUPLE_END)
+            if item == TUPLE_END:
+                break
+            the_tuple.append(item)
+        return tuple(the_tuple)
+
+    # pop: fn name
+
+    def pop_fn_name_and_returntype(self) -> tuple[str, str]:
+        name_and_returntype = self.pop_var_name_and_type()
+        assert isinstance(name_and_returntype, tuple) # make mypy happy
+        return name_and_returntype
+
+    def pop_fn_name(self, orr:None|str=None) -> str:
+        return self.pop_var_name(orr)
+
+    # pop: fn arg
+
+    def pop_fn_arg_begin(self) -> None:
+        fn_arg_begin = self.pop_var_name(justreturnif=FN_ARG_BEGIN)
+        assert fn_arg_begin == FN_ARG_BEGIN
+
+    def pop_fn_def_arg_or_end(self) -> None|tuple[str,str]:
+        name_and_type = self.pop_var_name_and_type(just_return_if_varname_is=FN_ARG_END)
+        if name_and_type == FN_ARG_END:
+            return None
+
+        assert isinstance(name_and_type, tuple)
+        # make mypy happy
+        
+        return name_and_type
+
+    def pop_fn_def_args(self) -> tuple[tuple[str,str], ...]:
+        self.pop_fn_arg_begin()
+
+        args = []
+        while True:
+            arg = self.pop_fn_def_arg_or_end()
+            if arg is None:
+                break
+            args.append(arg)
+
+        return tuple(args)
+
+    def pop_fn_call_args(self) -> tuple[str, ...]:
+        return self.pop_tuple()
+
+    # pop: fn body
+
+    def pop_fn_body_begin(self) -> None:
+        fn_body_begin = self.pop_var_name(justreturnif=FN_BODY_BEGIN)
+        assert fn_body_begin == FN_BODY_BEGIN
+
+    def pop_fn_body(self) -> str:
+        self.pop_fn_body_begin()
+
+        # TODO missing implementation
+
+        data = ''
+        while True:
+            fn_name = self.pop_fn_name(orr=FN_BODY_END)
+            if fn_name == FN_BODY_END:
+                break
+
+            fn_call_args = self.pop_fn_call_args()
+
+            data += f'{fn_name}({','.join(fn_call_args)});\n'
+
+        return data
 
 # main
 
@@ -211,8 +221,7 @@ def main() -> None:
 
     os.makedirs(FOLDER_TMP, exist_ok=True)
 
-    with open(FILE_INPUT, 'r') as f_in:
-        yasl_src = f_in.read()
+    src = Src(FILE_INPUT)
 
     with open(FILE_TMP_OUTPUT, 'w') as f_out:
 
@@ -221,12 +230,12 @@ def main() -> None:
 
         while True:
 
-            yasl_src = pop_whitespace(yasl_src)
+            src.pop_whitespace()
 
-            if len(yasl_src) == 0:
+            if src.no_more_code():
                 break
 
-            yasl_src, metatype = pop_var_metatype(yasl_src)
+            metatype = src.pop_var_metatype()
 
             match metatype:
 
@@ -235,7 +244,7 @@ def main() -> None:
 
                     # name and return type
 
-                    yasl_src, (name, ret_type) = pop_fn_name_and_returntype(yasl_src)
+                    name, ret_type = src.pop_fn_name_and_returntype()
 
                     f_out.write(f'__attribute__((warn_unused_result)) {ret_type} {name}')
                     # `-Wunused-result` doesn't do the trick
@@ -244,7 +253,7 @@ def main() -> None:
 
                     f_out.write('(')
 
-                    yasl_src, args = pop_fn_def_args(yasl_src)
+                    args = src.pop_fn_def_args()
 
                     args_str = ''
 
@@ -264,7 +273,7 @@ def main() -> None:
                     # body
 
                     f_out.write('\n{\n')
-                    yasl_src, body = pop_fn_body(yasl_src)
+                    body = src.pop_fn_body()
                     f_out.write(body)
                     f_out.write('\n}\n')
 
