496a17f48ff08a31e9dcab2df58f3df22568ec8e
kuche1 <znamsiparolata4esno@abv.bg>
2025-02-19 16:02:05 +0200

    update

--- yasl.py
+++ yasl.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python3
 
 import subprocess
-import enum
 import sys
 import os
 
@@ -32,19 +31,55 @@ ST_BEG_VAL = 'val'
 ST_BEG_VAR = 'var'
 ST_BEG_INC = 'inc'
 ST_BEG_DEC = 'dec'
-STATEMENT_BEGINNINGS = [ST_BEG_RET, ST_BEG_VAL, ST_BEG_VAR, ST_BEG_INC, ST_BEG_DEC]
+ST_BEG_CAST = 'cast'
+STATEMENT_BEGINNINGS = [ST_BEG_RET, ST_BEG_VAL, ST_BEG_VAR, ST_BEG_INC, ST_BEG_DEC, ST_BEG_CAST]
 
-# enum
+MT_FN_DEF = 'fn'
+MT_FN_DEC = 'fn@'
+METATYPES = [MT_FN_DEF, MT_FN_DEC]
 
-class VarMetatype(enum.Enum):
-    FN = 0
-    VAR = 1
-    VAL = 2
+MACRO_BODY_BEGIN = '('
+MACRO_BODY_END = ')'
+# right now those CAN be part of a variable name
+# I'm intentionally keeping this here, just to see what happens
 
-# generic fnc
+# fnc
 
 def term(args:list[str]) -> None:
     subprocess.run(args, check=True)
+
+def argtuple_to_ccallargs(args:tuple[str, ...]) -> str:
+    ret = ''
+
+    for arg_name in args:
+        c_arg_name = varname_to_cvarname(arg_name)
+        ret += f'{c_arg_name}, '
+
+    if ret.endswith(', '):
+        ret = ret[:-2]
+
+    return ret
+
+def argtuple_to_cdeclargs(args:tuple[tuple[str, str], ...]) -> str:
+    if len(args) == 0:
+        return 'void'
+
+    ret = ''
+    for arg_name, arg_type in args:
+        arg_name = varname_to_cvarname(arg_name)
+        ret += f'{arg_type} {arg_name}, '
+    if ret.endswith(', '):
+        ret = ret[:-2]
+    return ret
+
+def varname_to_cvarname(name:str) -> str:
+    # TODO! we're using this in more places than we should - in the future this is going to fuck strings over
+    # maybe we could omit the `$` to make the code a bit more readable and compliant
+    name = name.replace('-', '$M$')
+    name = name.replace('+', '$P$')
+    name = name.replace('(', '$OB$')
+    name = name.replace(')', '$CB$')
+    return name
 
 # class
 
@@ -58,7 +93,7 @@ class Src:
         
         self.line_number = 1
 
-        self.declared_functions:list[str] = ['printf'] # TODO hack # TODO ideally we would also check the types
+        self.declared_functions:list[str] = [] # TODO ideally we would also check the types
         self.called_functions:list[str] = []
 
     def no_more_code(self) -> bool:
@@ -70,7 +105,7 @@ class Src:
     
     def register_function_declaration(self, fn_name:str) -> None:
         if fn_name in self.declared_functions:
-            self.err(f'function `{fn_name}` already defined')
+            self.err(f'function `{fn_name}` already declared')
         self.declared_functions.append(fn_name)
     
     def register_function_call(self, fn_name:str) -> None:
@@ -119,8 +154,10 @@ class Src:
         if self.pop_var_name(orr=VAR_TYPE_SEP) != VAR_TYPE_SEP:
             self.err(f'expected a variable type seperator `{VAR_TYPE_SEP}` after `{name}`')
 
-    def pop_var_value(self) -> str:
-        value_or_fnccall = self.pop_var_name()
+    def pop_var_value_orr(self, orr:None|str) -> str:
+        value_or_fnccall = self.pop_var_name(orr=orr)
+        if value_or_fnccall == orr:
+            return value_or_fnccall
 
         fncargs = self.popif_tuple()
         if fncargs is None:
@@ -128,7 +165,12 @@ class Src:
             return value_or_fnccall
         
         # is a function call
-        return f'{value_or_fnccall}({', '.join(fncargs)})'
+        c_fn_name = varname_to_cvarname(value_or_fnccall)
+        c_fn_args = argtuple_to_ccallargs(fncargs)
+        return f'{c_fn_name}({c_fn_args})'
+
+    def pop_var_value(self) -> str:
+        return self.pop_var_value_orr(None)
 
     # pop: var name + misc
 
@@ -156,10 +198,6 @@ class Src:
 
         if len(data) == 0:
             return None
-
-        # this make the resulting C code less readable, maybe replace with M and P instead
-        data = data.replace('-', '$MINUS$')
-        data = data.replace('+', '$PLUS$')
 
         return data
 
@@ -198,19 +236,8 @@ class Src:
 
     # pop: var metatype
 
-    def pop_var_metatype(self) -> VarMetatype:
-        metatype = self.pop_var_name()
-
-        if metatype == 'fn':
-            mt = VarMetatype.FN
-        elif metatype == 'var':
-            mt = VarMetatype.VAR
-        elif metatype == 'val':
-            mt = VarMetatype.VAL
-        else:
-            raise Exception(f'unknown var metatype `{metatype}`')
-
-        return mt
+    def pop_var_metatype(self) -> str:
+        return self.pop_var_name()
 
     # pop: tuple
 
@@ -218,14 +245,19 @@ class Src:
         # TODO we're not taking care of string
         # TODO actually, anything with space doesnt work (like `(void * ) a` or `a + b`)
 
-        tuple_begin = self.pop_var_name(orr=TUPLE_BEGIN)
-        if tuple_begin != TUPLE_BEGIN:
-            self.unpop_var_name(tuple_begin)
+        self.pop_whitespace()
+
+        if self.no_more_code():
             return None
+
+        if self.src[0] != TUPLE_BEGIN:
+            return None
+    
+        self.src = self.src[1:]
 
         the_tuple = []
         while True:
-            item = self.pop_var_name(orr=TUPLE_END)
+            item = self.pop_var_value_orr(orr=TUPLE_END)
             if item == TUPLE_END:
                 break
             the_tuple.append(item)
@@ -250,9 +282,16 @@ class Src:
 
     # pop: fn arg
 
-    def pop_fn_arg_begin(self) -> None:
+    # returns True if error
+    def popif_fn_arg_begin(self) -> bool:
         fn_arg_begin = self.pop_var_name(orr=FN_ARG_BEGIN)
-        assert fn_arg_begin == FN_ARG_BEGIN
+        ret = fn_arg_begin != FN_ARG_BEGIN
+        if ret:
+            self.unpop_var_name(fn_arg_begin)
+        return ret
+
+    def pop_fn_arg_begin(self) -> None:
+        assert self.popif_fn_arg_begin() is False
 
     def pop_fn_def_arg_or_end(self) -> None|tuple[str,str]:
         name_and_type = self.pop_var_name_and_type_orr(orr=FN_ARG_END)
@@ -263,6 +302,19 @@ class Src:
         # make mypy happy
         
         return name_and_type
+
+    def popif_fn_def_args(self) -> None|tuple[tuple[str,str], ...]:
+        if self.popif_fn_arg_begin():
+            return None
+
+        args = []
+        while True:
+            arg = self.pop_fn_def_arg_or_end()
+            if arg is None:
+                break
+            args.append(arg)
+
+        return tuple(args)
 
     def pop_fn_def_args(self) -> tuple[tuple[str,str], ...]:
         self.pop_fn_arg_begin()
@@ -275,6 +327,13 @@ class Src:
             args.append(arg)
 
         return tuple(args)
+
+    def pop_fn_dec_args(self) -> str:
+        args = self.popif_fn_def_args()
+        if args is not None:
+            return argtuple_to_cdeclargs(args)
+
+        return self.pop_macro_body()
 
     def pop_fn_call_args(self, fn_name:str) -> tuple[str, ...]:
         return self.pop_tuple(f'could not get function `{fn_name}`\'s call args')
@@ -301,12 +360,14 @@ class Src:
 
             if statement_begin == ST_BEG_RET:
                 val_to_return = self.pop_var_name()
+                val_to_return = varname_to_cvarname(val_to_return)
                 return f'return {val_to_return};'
             
             # val or var
 
             if statement_begin in [ST_BEG_VAL, ST_BEG_VAR]:
                 var_name, var_type = self.pop_var_name_and_type()
+                var_name = varname_to_cvarname(var_name)
                 var_value = self.pop_var_value()
                 const_prefix = 'const ' if statement_begin == ST_BEG_VAL else '' # TODO you can't make gcc raise a warning if a variable was declared without const but was not modified, so we need to do something about this in the future
                 return f'{const_prefix}{var_type} {var_name} = {var_value};\n'
@@ -315,6 +376,7 @@ class Src:
 
             if statement_begin == ST_BEG_INC:
                 var_name = self.pop_var_name()
+                var_name = varname_to_cvarname(var_name)
                 inc_value = self.pop_var_name()
                 return f'{var_name} += {inc_value};\n'
 
@@ -322,15 +384,27 @@ class Src:
 
             if statement_begin == ST_BEG_DEC:
                 var_name = self.pop_var_name()
+                var_name = varname_to_cvarname(var_name)
                 dec_value = self.pop_var_name()
                 return f'{var_name} -= {dec_value};\n'
+            
+            # cast
+
+            if statement_begin == ST_BEG_CAST:
+                var = self.pop_var_name()
+                self.pop_var_type_sep(var)
+                new_c_type = self.pop_c_type()
+                cast_from = self.pop_var_value()
+                return f'{new_c_type} {var} = ({new_c_type}) {cast_from};\n' # TODO and what if it needs to be a constant ?
 
             # fn call
 
             if self.function_in_register(statement_begin):
-                self.register_function_call(statement_begin) # TODO this might becode useless very soon
+                self.register_function_call(statement_begin) # TODO! this might becode useless very soon
+                c_fn_name = varname_to_cvarname(statement_begin)
                 fn_call_args = self.pop_fn_call_args(statement_begin)
-                return f'{statement_begin}({', '.join(fn_call_args)});\n'
+                c_fn_args = argtuple_to_ccallargs(fn_call_args)
+                return f'{c_fn_name}({c_fn_args});\n'
             
             # invalid
 
@@ -349,6 +423,52 @@ class Src:
 
         return data
 
+    # pop: macro
+
+    def pop_macro_body(self) -> str:
+        self.pop_whitespace()
+
+        if self.no_more_code():
+            self.err(f'reached end of source code before macro beginning `{MACRO_BODY_BEGIN}` could be found')
+        
+        if self.src[0] != MACRO_BODY_BEGIN:
+            self.err(f'a macro beginning `{MACRO_BODY_BEGIN}` needs to follow, instead got `{self.src[0]}`')
+
+        self.src = self.src[1:]
+
+        end = self.src.find(MACRO_BODY_END)
+        if end == -1:
+            self.err(f'could not find macro end `{MACRO_BODY_END}`')
+        
+        macro = self.src[:end]
+        print(f'{macro=}')
+
+        print(f'before {self.src[:20]=}')
+        self.src = self.src[end+1:]
+        print(f'after  {self.src[:20]=}')
+
+        assert MACRO_BODY_BEGIN not in macro
+
+        return macro
+
+    # pop: c related
+
+    def pop_c_type(self) -> str:
+        data = ''
+        while not self.no_more_code():
+            ch = self.src[0]
+            self.src = self.src[1:]
+
+            if ch in WHITESPACE:
+                break
+        
+            data += ch
+                
+        assert len(data)
+
+        return data
+
+
 # main
 
 def main() -> None:
@@ -359,8 +479,8 @@ def main() -> None:
 
     with open(FILE_TMP_OUTPUT, 'w') as f_out:
 
-        f_out.write('#include <stdio.h>\n')
-        f_out.write('\n')
+        # f_out.write('#include <stdio.h>\n')
+        # f_out.write('\n')
 
         while True:
 
@@ -371,58 +491,50 @@ def main() -> None:
 
             metatype = src.pop_var_metatype()
 
-            match metatype:
+            if metatype == MT_FN_DEF:
 
-                case VarMetatype.FN:
-                    print('yeee function')
+                # name and return type
 
-                    # name and return type
+                fn_name, ret_type = src.pop_fn_name_and_returntype()
+                src.register_function_declaration(fn_name)
+                fn_name = varname_to_cvarname(fn_name)
 
-                    fn_name, ret_type = src.pop_fn_name_and_returntype()
+                f_out.write(f'__attribute__((warn_unused_result)) {ret_type} {fn_name}')
+                # `-Wunused-result` doesn't do the trick
 
-                    f_out.write(f'__attribute__((warn_unused_result)) {ret_type} {fn_name}')
-                    # `-Wunused-result` doesn't do the trick
+                # args
 
-                    src.register_function_declaration(fn_name)
+                f_out.write('(')
 
-                    # args
+                args = src.pop_fn_def_args()
 
-                    f_out.write('(')
+                f_out.write(argtuple_to_cdeclargs(args))
 
-                    args = src.pop_fn_def_args()
+                f_out.write(')')
 
-                    args_str = ''
+                # body
 
-                    if len(args) == 0:
-                        args_str += 'void'
-                    else:
-                        for arg_name, arg_type in args:
-                            args_str += f'{arg_type} {arg_name}, '
+                f_out.write('\n{\n')
+                body = src.pop_fn_body()
+                f_out.write(body)
+                f_out.write('\n}\n')
 
-                        if args_str.endswith(', '):
-                            args_str = args_str[:-2]
+            elif metatype == MT_FN_DEC:
 
-                    f_out.write(args_str)
+                fn_name, ret_type = src.pop_fn_name_and_returntype()
+                src.register_function_declaration(fn_name)
+                fn_name = varname_to_cvarname(fn_name)
 
-                    f_out.write(')')
+                fn_args = src.pop_fn_dec_args()
 
-                    # body
+                # TODO missing __attribute__((warn_unused_result))
+                # removed for now until I make a proper error handling system
+                f_out.write(f'{ret_type} {fn_name}({fn_args});\n')
+                # we could have used `()` but unfortunately this doesnt work for stdlib fncs (line printf)
 
-                    f_out.write('\n{\n')
-                    body = src.pop_fn_body()
-                    f_out.write(body)
-                    f_out.write('\n}\n')
-
-                case VarMetatype.VAR:
-                    print('yee var')
-                    raise NotImplementedError()
-
-                case VarMetatype.VAL:
-                    print('yeee val')
-                    raise NotImplementedError()
-
-                case _:
-                    assert False
+            else:
+                
+                src.err(f'unknown metatype `{metatype}`; valid metatypes are {METATYPES}')
 
     src.end_of_compilation_checks()
 
